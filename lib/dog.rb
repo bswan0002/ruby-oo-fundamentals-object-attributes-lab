c4f2b3adfad048cc099c72380ec5721329c2ba69
bswan0002 <bswan0002@gmail.com>
2020-11-17 13:44:52 -0800

    tests passed

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name= dogName
+    @name = dogName
+  end
+
+  def name
+    @name
+  end
+
+  def breed= breedName
+    @breed = breedName
+  end
+
+  def breed
+    @breed
+  end
+end
